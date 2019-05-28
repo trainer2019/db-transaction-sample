9ec4e7bc974dd66e4f3291ea050329fd6e2f5cb6
masahiro.ikeda <masahiro.ikeda@MyComputer>
2019-05-28 21:41:24 +0900

    トランザクション制御機能を追加

--- src/work/HamburgerShop.java
+++ src/work/HamburgerShop.java
@@ -1,5 +1,8 @@
 package work;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Scanner;
 
@@ -8,6 +11,16 @@ public class HamburgerShop {
 	// 商品一覧
 	private final static String[] ITEMS = { "ハンバーガー", "ポテト", "コーラ" };
 
+	// データベース接続用
+	private static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
+	private static final String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:ORCL";
+	private static final String USER_ID = "imuser";
+	private static final String USER_PASS = "impass";
+
+	/*
+	 * ハンバーガー購入プログラム
+	 * ( トランザクション制御 実装済み )
+	 */
 	public static void main(String[] args) {
 
 		// 標準入力の用意
@@ -28,32 +41,74 @@ public class HamburgerShop {
 			name = scan.nextLine();
 		} while (name.isEmpty());
 
-		// 注文ヘッダの登録
-		SalesHeadDao hDao = new SalesHeadDao();
-		SalesHeadDto hDto = new SalesHeadDto();
-		salesNo = hDao.getNextSalesNo();
-		hDto.setSalesNo(salesNo);
-		hDto.setCustomerName(name);
-		hDto.setCreatedAt(new Timestamp(System.currentTimeMillis()));
-		hDao.insertHead(hDto);
-
-		// 注文ラインの登録
-		System.out.println(name + " 様、ご注文をどうぞ。");
-		SalesLineDao lDao = new SalesLineDao();
-		for (String item : ITEMS) {
-			System.out.println(item + " はいくつ注文しますか？");
-
-			itemNumber = Integer.parseInt(scan.nextLine());
-			if (itemNumber > 0) {
-				SalesLineDto lDto = new SalesLineDto();
-				lDto.setSalesNo(salesNo);
-				lDto.setSubNo(++subNo);
-				lDto.setItem(item);
-				lDto.setItemNumber(itemNumber);
-				lDao.insertLine(lDto);
-			}
+		// JDBCドライバのロード
+		try {
+			Class.forName(DRIVER_NAME);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.exit(0);
 		}
 
-		System.out.println(name + " 様、ご注文ありがとうございました。");
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(JDBC_URL, USER_ID, USER_PASS);
+			con.setAutoCommit(false);
+
+			// 注文ヘッダの登録
+			SalesHeadDao hDao = new SalesHeadDao(con);
+			SalesHeadDto hDto = new SalesHeadDto();
+			salesNo = hDao.getNextSalesNo();
+			hDto.setSalesNo(salesNo);
+			hDto.setCustomerName(name);
+			hDto.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+			hDao.insertHead(hDto);
+
+			System.out.println(name + " 様、ご注文をどうぞ。");
+
+			// 注文ラインの登録
+			SalesLineDao lDao = new SalesLineDao(con);
+
+			for (String item : ITEMS) {
+				System.out.println(item + " はいくつ注文しますか？");
+
+				itemNumber = Integer.parseInt(scan.nextLine());
+				if (itemNumber > 0) {
+					SalesLineDto lDto = new SalesLineDto();
+					lDto.setSalesNo(salesNo);
+					lDto.setSubNo(++subNo);
+					lDto.setItem(item);
+					lDto.setItemNumber(itemNumber);
+					lDto.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+					lDao.insertLine(lDto);
+				}
+			}
+			con.commit();
+			System.out.println(name + " 様、ご注文ありがとうございました。");
+
+		} catch (NumberFormatException ne) {
+			ne.printStackTrace();
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		} catch (SQLException se) {
+			se.printStackTrace();
+			try {
+				con.rollback();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/work/SalesHeadDao.java
+++ src/work/SalesHeadDao.java
@@ -1,70 +1,48 @@
 package work;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SalesHeadDao {
 
-	// データベース接続に必要なデータ
-	private static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
-	private static final String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:ORCL";
-	private static final String USER_ID = "imuser";
-	private static final String USER_PASS = "impass";
+	// コネクション
+	private Connection con = null;
+
+	// コンストラクタでコネクションを受け取る
+	public SalesHeadDao(Connection con) {
+		this.con = con;
+	}
 
 	/**
 	 * 売上ヘッダデータを保存
 	 *
 	 * @param SalesHeadDto head
 	 */
-	public void insertHead(SalesHeadDto head) {
+	public void insertHead(SalesHeadDto head) throws SQLException {
 
-		try {
-			Class.forName(DRIVER_NAME);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		StringBuilder builder = new StringBuilder();
+		builder.append("INSERT INTO sales_head ( ");
+		builder.append("    sales_no,            ");
+		builder.append("    customer_name,       ");
+		builder.append("    created_at           ");
+		builder.append(") VALUES (               ");
+		builder.append("    ?,                   ");
+		builder.append("    ?,                   ");
+		builder.append("    ?                    ");
+		builder.append(")                        ");
+
+		PreparedStatement ps = con.prepareStatement(builder.toString());
+		ps.setInt(1, head.getSalesNo());
+		ps.setString(2, head.getCustomerName());
+		ps.setTimestamp(3, head.getCreatedAt());
+		ps.executeUpdate();
+
+		if (ps != null) {
+			ps.close();
 		}
 
-		Connection con = null;
-		PreparedStatement ps = null;
-
-		try {
-			con = DriverManager.getConnection(JDBC_URL, USER_ID, USER_PASS);
-
-			StringBuilder builder = new StringBuilder();
-			builder.append("INSERT INTO sales_head ( ");
-			builder.append("    sales_no,            ");
-			builder.append("    customer_name,       ");
-			builder.append("    created_at           ");
-			builder.append(") VALUES (               ");
-			builder.append("    ?,                   ");
-			builder.append("    ?,                   ");
-			builder.append("    ?                    ");
-			builder.append(")                        ");
-
-			ps = con.prepareStatement(builder.toString());
-			ps.setInt(1, head.getSalesNo());
-			ps.setString(2, head.getCustomerName());
-			ps.setTimestamp(3, head.getCreatedAt());
-
-			ps.executeUpdate();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (ps != null) {
-					ps.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 
 	/**
@@ -72,53 +50,30 @@ public class SalesHeadDao {
 	 *
 	 * @return 次の売上No
 	 */
-	public int getNextSalesNo() {
-
-		try {
-			Class.forName(DRIVER_NAME);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
+	public int getNextSalesNo() throws SQLException {
 
 		int nextNo = 0;
-		try {
-			con = DriverManager.getConnection(JDBC_URL, USER_ID, USER_PASS);
 
-			StringBuilder builder = new StringBuilder();
-			builder.append("SELECT                    ");
-			builder.append("  MAX(sales_no) AS max_no ");
-			builder.append("FROM                      ");
-			builder.append("  sales_head              ");
+		StringBuilder builder = new StringBuilder();
+		builder.append("SELECT                    ");
+		builder.append("  MAX(sales_no) AS max_no ");
+		builder.append("FROM                      ");
+		builder.append("  sales_head              ");
 
-			ps = con.prepareStatement(builder.toString());
-			rs = ps.executeQuery();
-
-			if (rs.next()) {
-				nextNo = rs.getInt("max_no");
-				nextNo += 1;
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (ps != null) {
-					ps.close();
-				}
-				if (rs != null) {
-					rs.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+		PreparedStatement ps = con.prepareStatement(builder.toString());
+		ResultSet rs = ps.executeQuery();
+		if (rs.next()) {
+			nextNo = rs.getInt("max_no");
+			nextNo += 1;
 		}
+
+		if (ps != null) {
+			ps.close();
+		}
+		if (rs != null) {
+			rs.close();
+		}
+
 		return nextNo;
 	}
 
